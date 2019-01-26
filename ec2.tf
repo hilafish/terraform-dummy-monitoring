e7507f852a8fbf79b040ca09f52234511dde423c
Hila Fish <hilavalensia@gmail.com>
2019-01-26 17:49:14 +0200

    Move kibana provisioning to user data

--- ec2.tf
+++ ec2.tf
@@ -56,6 +56,19 @@ data "template_file" "kibana_config" {
   }
 }
 
+data "template_file" "kibana-userdata" {
+  template = "${file("${path.module}/config/user-data/kibana-userdata.sh.tpl")}"
+
+  vars {
+    dummy_exporter-0 = "${aws_instance.dummy_exporter.0.private_dns}"
+    dummy_exporter-1 = "${aws_instance.dummy_exporter.1.private_dns}"
+    LOCAL_IPV4 = "$${LOCAL_IPV4}"
+    elastic_search_private_ip = "${aws_instance.elastic_search.private_ip}"
+    dummy1_hostname = "$${dummy1_hostname}"
+    dummy2_hostname = "$${dummy2_hostname}"
+  }
+}
+
 data "template_file" "prometheus-userdata" {
   template = "${file("${path.module}/config/user-data/prometheus-userdata.sh.tpl")}"
 
@@ -205,35 +218,7 @@ resource "aws_instance" "kibana_grafana" {
     destination = "/tmp/grafana_dashboard.json"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      # installing kibana & grafana
-      "wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -",
-      "echo \"deb https://artifacts.elastic.co/packages/6.x/apt stable main\" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list",
-      "echo \"deb https://packagecloud.io/grafana/stable/debian/ stretch main\" | sudo tee -a /etc/apt/sources.list.d/grafana.list",
-      "sudo curl https://packagecloud.io/gpg.key | sudo apt-key add -",
-      "sudo apt-get update",
-      "sudo apt-get install -y --allow-unauthenticated kibana grafana",
-      "sudo mv /tmp/kibana.yml /etc/kibana/",
-      "sudo sed -i \"s/#server.host: \\\"localhost\\\"/server.host: \\\"${self.private_ip}\\\"/g\" /etc/kibana/kibana.yml",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl enable kibana",
-      "sudo mkdir /var/lib/grafana/dashboards",
-      "sudo mv /tmp/prometheus_datasource.yaml /etc/grafana/provisioning/datasources/",
-      "sudo mv /tmp/grafana_dashboard.json /var/lib/grafana/dashboards/",
-      "sudo mv /tmp/prometheus_dashboards.yaml /etc/grafana/provisioning/dashboards/",
-      "dummy1_hostname=$(echo '${aws_instance.dummy_exporter.0.private_dns}' | awk '{split($0,a,\".\"); print a[1]}')",
-      "dummy2_hostname=$(echo '${aws_instance.dummy_exporter.1.private_dns}' | awk '{split($0,a,\".\"); print a[1]}')",
-      "sudo sed -i \"s/dummy-exporter-1/$${dummy1_hostname}/g\" /var/lib/grafana/dashboards/grafana_dashboard.json",
-      "sudo sed -i \"s/dummy-exporter-2/$${dummy2_hostname}/g\" /var/lib/grafana/dashboards/grafana_dashboard.json",
-      "sudo systemctl restart kibana",
-      "sudo systemctl enable grafana-server",
-      "sudo systemctl start grafana-server",
-      "sleep 30",
-      "curl -f -XPOST -H 'Content-Type: application/json' -H 'kbn-xsrf: anything' 'http://${self.private_ip}:5601/api/saved_objects/index-pattern/filebeat-*' '-d{\"attributes\":{\"title\":\"filebeat-*\",\"timeFieldName\":\"@timestamp\"}}'",
-      "curl -u elastic:${aws_instance.elastic_search.private_ip} -k -XPOST 'http://${self.private_ip}:5601/api/kibana/dashboards/import' -H 'Content-Type: application/json' -H \"kbn-xsrf: true\" -d @/tmp/kibana_dashboard.json",
-    ]
-  }
+  user_data = "${data.template_file.kibana-userdata.rendered}"
 }
 
 resource "aws_instance" "dummy_exporter" {
